b4c08efccd9909e46abd7997523c5e396aa0ad09
AleksanderGondek <gondekaleksander@hotmail.com>
2016-09-17 14:44:11 +0200

    Project wiped clean.

--- src/AleksanderGondek.ProjectBlitzkrieg.Mcts/GameStates/Examples/Chess/Models/ChessField.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AleksanderGondek.ProjectBlitzkrieg.Mcts.GameStates.Examples.Chess.Models
-{
-    public class ChessField
-    {
-        public BoardDefinitions.ChessPieces ChessPiece { get; set; }
-        public bool IsStartingPosition { get; set; }
-        public string Owner { get; set; }
-    }
-}
